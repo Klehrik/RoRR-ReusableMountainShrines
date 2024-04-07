59b4051f72791aea411d2f9b69e564183800b9ed
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-04-07 16:03:59 -0400

    v1.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,3 +6,6 @@
 
 ### v1.0.2
 * Added installation instructions to the main page.
+
+### v1.0.3
+* Updated all my mods to use a single helper file.
--- main.lua
+++ main.lua
@@ -1,40 +1,34 @@
--- Reusable Mountain Shrines v1.0.2
+-- Reusable Mountain Shrines v1.0.3
 -- Klehrik
 
 log.info("Successfully loaded ".._ENV["!guid"]..". Enjoy =)")
-require("./helper")
+mods.on_all_mods_loaded(function() for k, v in pairs(mods) do if type(v) == "table" and v.hfuncs then Helper = v end end end)
 
 
 
 -- ========== Main ==========
 
-function manage_shrines(mshrines)
-    if mshrines then
-        for i = 1, #mshrines do
-            local shrine = mshrines[i]
+function manage_shrines(shrines)
+    -- Reset mountain shrines after their animation finishes
+    for i = 1, #shrines do
+        local shrine = shrines[i]
 
-            -- Check for animation end
-            if shrine.image_index >= 14.0 and shrine.image_speed > 0 then
-                shrine.image_speed = -shrine.image_speed
+        -- Check for animation end
+        if shrine.image_index >= 14.0 and shrine.image_speed > 0 then
+            shrine.image_speed = -shrine.image_speed
 
-            -- Check for reverse animation end
-            elseif shrine.image_index <= 0.5 and shrine.image_speed < 0 then
-                shrine.active = 0.0
-                shrine.image_speed = 0.0
-                shrine.image_index = 0.0
+        -- Check for reverse animation end
+        elseif shrine.image_index <= 0.5 and shrine.image_speed < 0 then
+            shrine.active = 0.0
+            shrine.image_speed = 0.0
+            shrine.image_index = 0.0
 
-            end
         end
     end
 end
 
 
 gm.pre_script_hook(gm.constants.__input_system_tick, function()
-    -- Reset mountain shrines after their animation finishes
-    
-    local mshrines = find_all_cinstance_type(gm.constants.oShrineMountain)
-    manage_shrines(mshrines)
-
-    mshrines = find_all_cinstance_type(gm.constants.oShrineMountainS)
-    manage_shrines(mshrines)
+    manage_shrines(Helper.find_active_instance_all(gm.constants.oShrineMountain))
+    manage_shrines(Helper.find_active_instance_all(gm.constants.oShrineMountainS))
 end)
